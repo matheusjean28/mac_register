c3927bd913a7057442a73f1e808b177d2463374b
matheusjean28 <matheusjean28@gmail.com>
2023-09-02 11:09:14 -0300

    route upload is avaliable now, but not save file on DB

--- Controllers/UploadController.cs
+++ Controllers/UploadController.cs
@@ -7,29 +7,32 @@ namespace ControllerUpload
 {
     [ApiController]
     [Route("upload")]
-
     public class UploadController : ControllerBase
     {
-        public async Task<ActionResult> Upload([FromForm] ICollection<IFormFile> files)
+    [HttpPost]
+        public async Task<ActionResult> Upload([FromForm] ICollection<IFormFile>? files )
         {
             if (files == null || files.Count == 0)
             {
                 return BadRequest();
-            }
 
+            }
             List<byte[]> data = new();
             foreach (var formFile in files)
             {
                 if (formFile.Length > 0)
                 {
-                    using (var stream = new MemoryStream())
-                    {
-                        await formFile.CopyToAsync(stream);
-                        data.Add(stream.ToArray());
-                    }
+                    using var stream = new MemoryStream();
+                    await formFile.CopyToAsync(stream);
+                    data.Add(stream.ToArray());
                 }
             }
-            return File(data[0], files.FirstOrDefault().ContentType, "myfile.txt");
+            if (data.Count > 0 && files.FirstOrDefault() != null)
+            {
+                return Ok();
+            }
+
+            return BadRequest("No valid files found.");
         }
     }
 }
