26c6132ac9ca028e7a3de6172a60bd8889db733c
matheusjean28 <matheusjean28@gmail.com>
2023-09-02 10:35:21 -0300

    Upload branch add files

--- Program.cs
+++ Program.cs
@@ -1,32 +1,34 @@
 using Microsoft.EntityFrameworkCore;
 using DeviceModel;
 using DeviceContext;
-using ControllerUpload;
-
-using Microsoft.Extensions.DependencyModel;
-using System.Runtime.CompilerServices;
-
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<DeviceDb>(opt => opt.UseSqlite("Data Source=Mac.db"));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
 var app = builder.Build();
-app.UseStaticFiles();
-app.MapControllers();
+app.UseSwagger(options =>
+{
+    options.SerializeAsV2 = true;
+});
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
 
 
 app.MapGet("/mac", async (DeviceDb db) =>
     await db.Devices.ToListAsync());
-app.MapPost("/upload", async (ICollection<IFormFile> files, UploadController uploadController) =>
-{
-    return await uploadController.Upload(files);
-});
 
 app.MapPost("/mac", async (Device device, DeviceDb db) =>
 {
     db.Devices.Add(device);
     await db.SaveChangesAsync();
-
     return Results.Created("/mac", device);
 });
 
