946c1d2a110c49a396447249ff0b4c4562c9f9bf
matheusjean28 <matheusjean28@gmail.com>
2023-09-28 21:10:28 -0300

    start to implement queues

---
+++ Services/ProcessStatus.cs
@@ -0,0 +1,8 @@
+namespace ProcessMessageServices
+{
+    public class ProcessStatus
+    {
+        public string Status { get; set; } = string.Empty;
+        public int Progress { get; set; }
+    }
+}
