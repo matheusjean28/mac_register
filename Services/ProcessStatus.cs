dbd1e4a2e91c9010dc2ff40950ff1c1e9111b43d
matheusjean28 <matheusjean28@gmail.com>
2023-09-28 21:11:18 -0300

    line

--- Services/ProcessStatus.cs
+++ Services/ProcessStatus.cs
@@ -3,6 +3,7 @@ namespace ProcessMessageServices
     public class ProcessStatus
     {
         public string Status { get; set; } = string.Empty;
+        
         public int Progress { get; set; }
     }
-}
+} 
