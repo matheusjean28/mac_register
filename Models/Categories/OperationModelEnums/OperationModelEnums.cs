d55f32cb968000c2a1277534f68bcd85b763c222
matheusjean28 <matheusjean28@gmail.com>
2024-06-29 15:40:57 -0300

    feature model table at database

---
+++ Models/Categories/OperationModelEnums/OperationModelEnums.cs
@@ -0,0 +1,11 @@
+namespace MacSave.Models.Categories.OperationModelEnums
+{
+	public enum OperationModelEnum
+    {
+        Router = 0,
+        Bridge = 1,
+        RouterAndBrige = 2,
+        
+        None = 3
+	}
+}
