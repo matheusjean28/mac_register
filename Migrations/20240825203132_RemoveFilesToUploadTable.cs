9c0c90456ca474bbe9fecb91f701bc5ff5025d27
matheusjean28 <matheusjean28@gmail.com>
2024-08-25 17:36:08 -0300

    remove and add again files to database

---
+++ Migrations/20240825203132_RemoveFilesToUploadTable.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MacSave.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveFilesToUploadTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FilesUploads");
+
+            migrationBuilder.DropTable(
+                name: "MacstoDbs");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FilesUploads",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Data = table.Column<byte[]>(type: "BLOB", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FilesUploads", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MacstoDbs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Mac = table.Column<string>(type: "TEXT", nullable: false),
+                    Model = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    RemoteAccess = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MacstoDbs", x => x.Id);
+                });
+        }
+    }
+}
---
+++ Migrations/20240825203519_AddFilesToDatabaseAgain.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MacSave.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFilesToDatabaseAgain : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FilesUploads",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Data = table.Column<byte[]>(type: "BLOB", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FilesUploads", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MacstoDbs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Model = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Mac = table.Column<string>(type: "TEXT", nullable: false),
+                    RemoteAccess = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MacstoDbs", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FilesUploads");
+
+            migrationBuilder.DropTable(
+                name: "MacstoDbs");
+        }
+    }
+}
